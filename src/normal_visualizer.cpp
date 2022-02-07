c3ed10ae7c5229baa4e12e833528f0d0ef64719f
Takashi Amada <octo-enigma@outlook.com>
2022-02-06 23:59:13 -0800

    Added normal visualizer for debug

---
+++ src/normal_visualizer.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <cmath>
+
+#include "vecmath.h"
+#include "triangle.h"
+#include "camera.h"
+#include "ray.h"
+#include "bvh.h"
+#include "image.h"
+#include "normal_visualizer.h"
+
+
+namespace prt
+{
+
+void NormalVisualizer::TraceBlock(Image& image, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, const Scene& scene, const Camera& camera)
+{
+    for (uint32_t y = y0; y <= y1; y++) {
+        for (uint32_t x = x0; x <= x1; x++) {
+            Vector3f color = Trace(camera, scene, x, y);
+
+            image.WritePixel(x, y, color);
+        }
+    }
+}
+
+Vector3f NormalVisualizer::Trace(const Camera& camera, const Scene& scene, uint32_t x, uint32_t y)
+{
+    Vector3f color(0);
+
+    auto ray = camera.GenerateJitteredRay(m_rand, x, y);
+    RayIntersection intr;
+    scene.intersect(intr, ray);
+
+    if (intr.isHit()) {
+        SurfaceProperties prop;
+        scene.getSurfaceProperties(prop, intr);
+        color = prop.normal*0.5f + 0.5f;
+    }
+
+    return color;
+}
+
+} // namespace prt
---
+++ src/normal_visualizer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "vecmath.h"
+#include "random.h"
+
+namespace prt
+{
+
+class Scene;
+struct Material;
+class Image;
+class Camera;
+
+class NormalVisualizer
+{
+public:
+    NormalVisualizer() {}
+
+    void TraceBlock(Image& image, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, const Scene& scene, const Camera& camera);
+    Vector3f Trace(const Camera& camera, const Scene& scene, uint32_t x, uint32_t y);
+
+private:
+    Random m_rand;
+};
+
+} // namespace prt

b997b023b128a8bcb5917733b5887c3a39739cb8
Takashi Amada <octo-enigma@outlook.com>
2022-04-02 21:05:16 -0700

    Fixed camera screen axes

--- src/camera.cpp
+++ src/camera.cpp
@@ -13,12 +13,13 @@ Ray Camera::GenerateJitteredRay(Random& rand, uint32_t x, uint32_t y) const
 {
     Ray ray;
     const float screenScale = 0.6f;
+
     float s = 0.125f*m_invWidth;
     float dx = rand.generateMinus1to1() * s;
     float dy = rand.generateMinus1to1() * s;
     float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*screenScale;
     float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*screenScale;
-    auto d = normalize(Vector3f(nx, ny, 0) + m_dir);
+    auto d = normalize(nx*m_right + ny*m_up + m_dir);
 
     ray.maxT = 100000.0f;
     ray.org = m_pos;
@@ -42,7 +43,7 @@ SoaRay Camera::GenerateJitteredSoaRay(Random& rand, uint32_t x, uint32_t y) cons
         float dy = rand.generateMinus1to1() * s;
         float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*screenScale;
         float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*screenScale;
-        dir[i] = normalize(Vector3f(nx, ny, 0) + m_dir);
+        dir[i] = normalize(nx*m_right + ny*m_up + m_dir);
     }
 
     ray.maxT = 100000.0f;
--- src/camera.h
+++ src/camera.h
@@ -13,10 +13,21 @@ class Camera
 {
 public:
     Camera(const Vector3f& pos, const Vector3f& dir, uint32_t width, uint32_t height)
-     : m_pos(pos), m_dir(dir), m_width(width), m_height(height)
+     : m_pos(pos), m_dir(normalize(dir)), m_width(width), m_height(height)
     {
         m_invWidth = 1.0f/width;
         m_invHeight = 1.0f/height;
+
+        auto up = Vector3f(0, 1.0f, 0);
+        auto right = cross(dir, up);
+        if (length(right) < 0.00001f) {
+            right = cross(dir, Vector3f(1, 0, 0));
+        }
+        right = normalize(right);
+        up = normalize(cross(right, dir));
+
+        m_up = up;
+        m_right = right;
     }
 
     Ray GenerateJitteredRay(Random& rand, uint32_t x, uint32_t y) const;
@@ -24,6 +35,8 @@ public:
 
     Vector3f m_pos;
     Vector3f m_dir;
+    Vector3f m_up;
+    Vector3f m_right;
     uint32_t m_width;
     uint32_t m_height;
     float m_invWidth;

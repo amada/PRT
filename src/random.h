bdae99cd3e900884c13d2fb6fd70f8d8c3b2eade
Takashi Amada <octo-enigma@outlook.com>
2022-04-23 10:16:35 -0700

    Switched to RNG based on Xorshift

--- src/random.h
+++ src/random.h
@@ -7,6 +7,51 @@
 namespace prt
 {
 
+#if 1
+
+class Random
+{
+public:
+    Random() {
+        m_state.a = rand() | 1;
+    }
+
+    float generateMinus1to1() {
+        return 2.0f*generate() - 1.0f;
+    }
+
+    float generate() {
+        auto u = xorshift32(&m_state);
+        union {
+            uint32_t u;
+            float f;
+        } ui;
+
+        ui.u = (u & 0x007fffff) | 0x3f800000;
+
+        return ui.f - 1.0f;
+    }
+
+private:
+    // Using https://en.wikipedia.org/wiki/Xorshift
+    struct xorshift32_state {
+        uint32_t a;
+    };
+
+    uint32_t xorshift32(xorshift32_state *state)
+    {
+        auto x = state->a;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        return state->a = x;
+    }
+
+    xorshift32_state m_state;
+};
+
+#else
+
 class Random
 {
 public:
@@ -25,5 +70,7 @@ private:
     std::mt19937 m_gen;
     std::uniform_real_distribution<float> m_rand;
 };
+
+#endif
 
 } // namespace prt

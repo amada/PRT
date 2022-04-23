1f110427992a44bb9e6fea6d092f8ade8c9baf3f
Takashi Amada <octo-enigma@outlook.com>
2022-04-23 10:14:22 -0700

    Support for aspect ratio other than 1:1

--- src/camera.cpp
+++ src/camera.cpp
@@ -12,13 +12,15 @@ namespace prt
 Ray Camera::GenerateJitteredRay(Random& rand, uint32_t x, uint32_t y) const
 {
     Ray ray;
-    const float screenScale = 0.6f;
+    const float kScreenScale = 0.6f;
+    const float kAspect = (float)m_width/m_height;
+    const float kScaleX = 0.5f*m_invWidth;
+    const float kScaleY = 0.5f*m_invHeight;
 
-    float s = 0.125f*m_invWidth;
-    float dx = rand.generateMinus1to1() * s;
-    float dy = rand.generateMinus1to1() * s;
-    float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*screenScale;
-    float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*screenScale;
+    float dx = rand.generateMinus1to1()*kScaleX;
+    float dy = rand.generateMinus1to1()*kScaleY;
+    float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*kScreenScale*kAspect;
+    float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*kScreenScale;
     auto d = normalize(nx*m_right + ny*m_up + m_dir);
 
     ray.maxT = 100000.0f;
@@ -36,18 +38,21 @@ RayPacket Camera::GenerateJitteredRayPacket(Random& rand, uint32_t x, uint32_t y
 
     Vector3f avgDir(0.0f);
 
+    const float kScreenScale = 0.6f;
+    const float kAspect = (float)m_width/m_height;
+    const float kScaleX = 0.5f*m_invWidth;
+    const float kScaleY = 0.5f*m_invHeight;
+
     for (uint32_t v = 0; v < RayPacket::kVectorCount; v++) {
         SoaRay ray;
-        const float screenScale = 0.6f;
-        float s = 0.125f*m_invWidth;
 
         Vector3f dir[SoaConstants::kLaneCount];
 
         for (uint32_t i = 0; i < SoaConstants::kLaneCount; i++) {
-            float dx = rand.generateMinus1to1() * s;
-            float dy = rand.generateMinus1to1() * s;
-            float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*screenScale;
-            float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*screenScale;
+            float dx = rand.generateMinus1to1()*kScaleX;
+            float dy = rand.generateMinus1to1()*kScaleY;
+            float nx = 2.0f*(x*m_invWidth - 0.5f + dx)*kScreenScale*kAspect;
+            float ny = -2.0f*(y*m_invHeight - 0.5f + dy)*kScreenScale;
             dir[i] = normalize(nx*m_right + ny*m_up + m_dir);
             avgDir = avgDir + dir[i];
         }
